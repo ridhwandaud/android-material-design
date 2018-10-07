5cde334429a6b5432f66e81b2f76930e7eec5b6d
Ridhwan <muhammadridhwandaud@gmail.com>
2018-10-07 11:32:09 +0800

    add basic material design

---
+++ app/src/main/java/com/ridhwandaud/todo/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.ridhwandaud.todo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
